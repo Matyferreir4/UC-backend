427a4160c1831bb01de43da9477ea72054c75185
MATHEUS HENRIQUE DO NASCIMENTO FERREIRA <matheus.hnferreira@senacsp.edu.br>
2024-12-03 21:49:20 -0300

    Aula dia 03-12-24

--- app.js
+++ app.js
@@ -10,6 +10,53 @@ app.get('/', (requisicao, resposta) =>{
     resposta.status(200).send(carros2024);
 });
 
+app.get ('/:sigla', (requisicao, resposta) => {
+    const siglainformada = requisicao.params.sigla.toUpperCase();
+    const carro = carros2024.find((infoCarro) => infoCarro.sigla === siglainformada)
+
+    if (!carro){
+        //Caso do campo estar vazio
+        resposta.status(404).send('Não existe sigla informada')
+        return;
+    };
+    resposta.status(200).send(carro); // se encontrada a sigla, retorna a tabela
+})
+
+app.post ('/', (req, res) => {
+    const NovoCarro = req.body;
+    carros2024.push(NovoCarro);
+    res.status(200).send(NovoCarro);
+}
+
+)
+
+app.put('/:sigla', (req, res) => {
+    const siglainformada = req.params.sigla.toUpperCase();
+    const carroselecionado = carros2024.find(c => c.sigla === siglainformada);
+    if (!carroselecionado) {
+        res.status(404).send('Não existe um carro com a sigla informada');
+        return;
+    };
+    const campos = Object.keys(req.body);
+    for (let campo of campos) {
+        carroselecionado[campo] = req.body[campo];
+    }
+    res.status(200).send(carroselecionado)
+    });
+
+app.delete('/:sigla', (req,res) => {
+    const siglainformada = req.params.sigla.toUpperCase();
+    const indicecarroselecionado = carros2024.findIndex(
+        (c) => c.sigla === siglainformada
+    );
+    if (indicecarroselecionado === -1) {
+        res.status(404).send('Não existe um carro com a sigla informada');
+        return;
+    };
+    const carroremovido = carros2024.splice(indicecarroselecionado, 1);
+    res.status(200).send(carroremovido);
+})
+
 
 // inicia o servidor na porta 3000
 app.listen(3000,() => console.log ("servidor Rodando Com Sucesso"));
--- tabelacarros.js
+++ tabelacarros.js
@@ -17,6 +17,24 @@ const carros2024 = [
         aceleracao: 2.8,
         preco: 330000,
     },
+    {
+        nome: 'Land Rover Evoque',
+        sigla: 'LRE',
+        velocidadeMaxima: 200,
+        potencia: 200,
+        consumo: 7.0,
+        aceleracao: 4.0,
+        preco: 300000
+    },
+    {
+        nome: 'Corsa 2011',
+        sigla: 'CS11',
+        velocidadeMaxima: 160,
+        potencia: 100,
+        consumo: 13.0,
+        aceleracao: 5.0,
+        preco: 10000
+    }
 ];
 
 export default carros2024;
